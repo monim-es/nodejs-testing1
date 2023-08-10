02aca0dba376788d0e48da0c9164331203ea76f8
momo <monimsrw@outlook.fr>
2023-08-10 13:41:06 +0100

    initial commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const {v4} = require('uuid')
+
+
+console.log(v4());
+
+// const http = require('http')
+
+
+// const server = http.createServer((req, res)=> {
+//     res.statusCode = 200
+//     res.setHeader('Content-Type', 'text/html')
+//     res.write('<h1> welcome </h1> <p> momo es</p>')
+//     res.end()
+  
+// })
+
+// server.listen(3000, '127.0.0.1', ()=> {
+//   console.log('server runnning ....');
+// })
+
+
